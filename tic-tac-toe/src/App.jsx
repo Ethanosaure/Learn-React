d8a120287b78005cddc63d9bbe68876cb02a0cb1
Ethanosaure <ethandiasmarques.pro@gmail.com>
2023-09-05 15:09:53 +0200

    adding the grid for the game

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,7 +1,33 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+function Square({ value }) {
+  function handleClick() {
+    console.log("Clicked");
+  }
+  return (
+    <button onClick={handleClick} className="square">
+      {value}
+    </button>
+  );
+}
 
-export default function Square() {
-  return <button className="square">X</button>;
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </>
+  );
 }
