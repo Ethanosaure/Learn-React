3eac548ad9c1684f1cf93e29bc1be748727844a0
Ethanosaure <ethandiasmarques.pro@gmail.com>
2023-09-05 14:53:41 +0200

    adding first box for the tic tac toe

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,7 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+export default function Square() {
+  return <button className="square">X</button>;
+}
