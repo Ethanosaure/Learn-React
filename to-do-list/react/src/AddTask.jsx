daa40d5738c80cd13ba8d7e9efefd0fa46f1f80d
Ethanosaure <ethandiasmarques.pro@gmail.com>
2023-09-18 11:14:26 +0200

    adding to do list base

---
+++ to-do-list/react/src/AddTask.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function AddTask() {
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <input type="date" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
+export default AddTask;
